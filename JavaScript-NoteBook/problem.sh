f4e64ba47c4535284a357d5b777dceba4278e441
baudelee <baudelee@gmail.com>
2017-06-01 16:05:17 +0800

    Update problem.sh

--- JavaScript-NoteBook/problem.sh
+++ JavaScript-NoteBook/problem.sh
@@ -1,6 +1,7 @@
 - 回调函数
 
 Related Website for Learning
+http://www.cnblogs.com/moltboy/archive/2013/04/24/3040213.html
 
 Bootstrap中文网：
 // 包含相关的js库、构建工具等
@@ -8,3 +9,7 @@ http://www.bootcss.com/
 
 Undercore.js
 http://www.cnblogs.com/chaojidan/p/4156002.html
+
+闭包:
+http://www.ruanyifeng.com/blog/2009/08/learning_javascript_closures.html
+http://wlog.cn/javascript/javascript-closures.html
